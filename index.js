b06c5e1958e554526ed7cb2b588fd1f3f5c7f7f2
fileofish <oleg.b9113@gmail.com>
2024-03-19 14:33:04 +0600

    feat: create iframe

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const iframe = document.createElement('iframe');
+const button = document.createElement('button');
 
-iframe.setAttribute('src', 'https://dialogsravnovesiedev.netlify.app/?title=111');
-iframe.style.width = '100%';
-iframe.style.height = '100%';
-iframe.style.position = 'fixed';
-iframe.style.top = '0';
-iframe.style.left = '0';
-iframe.style.border = 'none';
-iframe.style.zIndex = '1000';
+button.innerHTML = 'Hi!'
+button.style.position = 'fixed';
+button.style.top = '0';
+button.style.right = '0';
+button.style.width = '100px';
+button.style.height = '100px';
 
 document.body.prepend(iframe);
+
+
+// const iframe = document.createElement('iframe');
+
+// iframe.setAttribute('src', 'https://dialogsravnovesiedev.netlify.app/?title=111');
+// iframe.style.width = '100%';
+// iframe.style.height = '100%';
+// iframe.style.position = 'fixed';
+// iframe.style.top = '0';
+// iframe.style.left = '0';
+// iframe.style.border = 'none';
+// iframe.style.zIndex = '1000';
+
+// document.body.prepend(iframe);
